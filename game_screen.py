1344e169ac724964e68f03b1a3726ad38f358c11
Greg M. Krsak <greg.krsak@gmail.com>
2023-11-25 09:54:30 -0800

    Fixes #2

--- game_screen.py
+++ game_screen.py
@@ -10,9 +10,16 @@ class GameScreen(View):
 	Represents the screen that the game plays on.
 	"""
 
+	# Default background color is black
+	background_color = (0, 0, 0)
+
 	def __init__(self, width: int, height: int):
 		super().__init__()
 		self.output = pygame.display.set_mode((width, height))
+
+	# Clears the screen to black
+	def clear(self):
+		self.output.fill(self.background_color)
 
 	# Call this to update the screen with the latest changes
 	def tick(self):
