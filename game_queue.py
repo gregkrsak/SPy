d21dd2a69a8af37a4db5f7ea666bfe04199fb873
Greg M. Krsak <greg.krsak@gmail.com>
2023-12-09 15:11:34 -0800

    Fixes #10 - Remove debug statements

--- game_queue.py
+++ game_queue.py
@@ -15,9 +15,7 @@ class GameQueue(Model, queue.SimpleQueue):
 
 	def __init__(self, controller: Controller, callback: Callable[..., None]):
 		super().__init__(view = None)
-		print(f'GameQueue {self.unique_id} initializing...')
 		self._worker_callback = callback
-		print(f'GameQueue {self.unique_id} callback = {self._worker_callback}')
 		self.controller = controller
 		# FIXME: Uncomment this
 		#self.controller.model = self
@@ -26,9 +24,7 @@ class GameQueue(Model, queue.SimpleQueue):
 			while True:
 				next_item = self.get()
 				## Do work here ##
-				print(f'GameQueue {self.unique_id} working on {next_item}...')
 				self._worker_callback(next_item)
-				print(f'GameQueue {self.unique_id} completed {next_item}')
 				##################
 
 		threading.Thread(target = worker, daemon = True).start()
